aac6df89e4d61b3c60b084e58f8408b155a6f756
sjai <jai.shankar@sosuvconsulting.com>
2024-04-30 17:36:57 +0530

    Updatede the API

--- app.js
+++ app.js
@@ -14,8 +14,11 @@ export const app = express();
 // Using Middlewares
 app.use(express.json());
 app.use(cookieParser());
+
+
 app.use(
   cors({
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     origin: [process.env.FRONTEND_URI_1, process.env.FRONTEND_URI_2],
